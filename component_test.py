2880ce33c3199680a8af1fd92e44b2e439ca50dc
Vennadio <kirik550088@gmail..com>
2024-04-07 16:08:24 +0300

    adding2

--- component_test.py
+++ component_test.py
@@ -1,35 +1,6 @@
-from fastapi import HTTPException
-from requests.exceptions import RequestException
-from unittest.mock import MagicMock
-import pytest
+import routes_service
 
-from your_module import app, FIRST_SERVICE_URL, process_stops
+def test_syntax():
+    import routes_service
 
-@pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch('/processed_stops/')
-    yield mock
-
-def test_process_stops(mock_requests_get):
-    # Устанавливаем мок для успешного запроса
-    mock_requests_get.return_value.json.return_value = [{"name": "stop1", "location": "location1"}, {"name": "stop2", "location": "location2"}]
-
-    # Вызываем функцию для обработки остановок
-    processed_stops = process_stops()
-
-    # Проверяем, что запрос был отправлен к правильному URL
-    mock_requests_get.assert_called_once_with(FIRST_SERVICE_URL)
-
-    # Проверяем, что данные обработаны правильно
-    assert processed_stops == [{"name": "STOP1", "location": "location1"}, {"name": "STOP2", "location": "location2"}]
-
-def test_process_stops_error(mock_requests_get):
-    # Устанавливаем мок для исключения при запросе
-    mock_requests_get.side_effect = RequestException("Test error")
-
-    # Проверяем, что возникает HTTPException при ошибке запроса
-    with pytest.raises(HTTPException) as exc_info:
-        process_stops()
-
-    assert exc_info.value.status_code == 500
-    assert "Error while fetching stops data" in str(exc_info.value)
+    assert True
