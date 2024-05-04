7630c44853c71033ddbe4c8258a5bea35ab803a6
Vennadio <kirik550088@gmail..com>
2024-05-04 10:37:26 +0300

    adding new1

--- component_test.py
+++ component_test.py
@@ -1,6 +1,23 @@
 import routes_service
+import unittest
+import requests
 
-def test_syntax():
-    import routes_service
+class TestYourService(unittest.TestCase):
+    def setUp(self):
+        pass
 
-    assert True
+    def tearDown(self):
+        pass
+
+    def test_processed_stops_endpoint(self):
+
+        url = "http://localhost:8000/processed_stops/" 
+        response = requests.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTrue(isinstance(response.json(), list))
+
+if __name__ == '__main__':
+    unittest.main()
+
--- test_main.py
+++ test_main.py
@@ -17,7 +17,14 @@ class TestSyntax(unittest.TestCase):
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
 
-        # Проверяем, что код успешно загружен
+        self.assertIsNotNone(module)
+    
+    def check_response(self):
+        file_path = Path("routes_service.py")
+        spec = importlib.util.spec_from_file_location(file_path.stem, file_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+
         self.assertIsNotNone(module)
 
 if __name__ == "__main__":
