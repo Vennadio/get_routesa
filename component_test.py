1a7caddfd971e294e6e0a688eaffc8d33a8d38d0
Vennadio <kirik550088@gmail..com>
2024-05-04 10:48:42 +0300

    adding new1

--- component_test.py
+++ component_test.py
@@ -10,11 +10,6 @@ class TestTabulation(unittest.TestCase):
             for idx, line in enumerate(lines, start=1):
                 self.assertTrue(re.match(r'^\t', line), f"Tabulation missing in line {idx}")
 
-class TestHTTP(unittest.TestCase):
-
-    def test_process_stops_http(self):
-        response = requests.get("http://localhost:8000/processed_stops/") 
-        self.assertEqual(response.status_code, 200, "HTTP request failed")
 
 if __name__ == '__main__':
     unittest.main()
