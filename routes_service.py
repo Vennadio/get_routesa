737f1448f9ebccca04148c1ae9f4e9c25f80afa8
Vennadio <kirik550088@gmail..com>
2024-05-04 10:25:21 +0300

    adding new

--- routes_service.py
+++ routes_service.py
@@ -9,17 +9,14 @@ FIRST_SERVICE_URL = "http://stops-service:8000/stops/"
 @app.get("/processed_stops/")
 def process_stops():
     try:
-        # Получаем данные об остановках из первого сервиса
         response = requests.get(FIRST_SERVICE_URL)
-        response.raise_for_status()  # Проверяем успешность запроса
+        response.raise_for_status() 
 
         stops_data = response.json()
 
-        # Обработка данных
         processed_stops = []
 
         for stop in stops_data:
-            # Например, преобразование названия остановки в верхний регистр
             processed_stop = {
                 "name": stop["name"].upper(),
                 "location": stop["location"]
@@ -29,5 +26,4 @@ def process_stops():
         return processed_stops
 
     except requests.RequestException as e:
-        # В случае ошибки при запросе к первому сервису
         raise HTTPException(status_code=500, detail=f"Error while fetching stops data: {str(e)}")
